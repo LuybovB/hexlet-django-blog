38f681f459be1d2edb680299a6cf8077caab3275
LuybovB <grandprinceess7777@gmail.com>
2024-05-24 21:19:58 +0300

    migrations

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -4,4 +4,6 @@ from hexlet_django_blog.article.views import CustomIndexView
 
 urlpatterns = [
     path('', CustomIndexView.as_view()),
+    path('tags/<str:tags>/<int:article_id>/', CustomIndexView.as_view()),  # Маршрут с параметрами tags и article_id
+
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -5,12 +5,18 @@ from django.views import View
 class CustomIndexView(View):
     template_name = 'articles/index.html'
 
-    def get_context_data(self):
+    def get_context_data(self, tags=None, article_id=None):  # Добавляем параметры tags и article_id
         context = {
-            'app_name': 'hexlet_django_blog.article',  # Здесь можно передать динамическое значение
+            'app_name': 'hexlet_django_blog.article',
+            'tags': tags,  # Пример добавления tags в контекст
+            'article_id': article_id,  # Пример добавления article_id в контекст
         }
         return context
 
-    def get(self, request):
-        context = self.get_context_data()
+    def get(self, request, tags=None, article_id=0):
+        tags = tags if tags else 'python'
+        article_id = article_id if article_id else 42
+        context = self.get_context_data(tags, article_id)
         return render(request, self.template_name, context)
+
+
